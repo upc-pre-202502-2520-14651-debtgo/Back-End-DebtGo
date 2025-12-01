379331253d0542310f3f8d67b373ebadf9266e90
Developer <danielj.chavarriz@gmail.com>
2025-12-01 15:11:15 -0500

    feat(debtgo): update back-end

--- src/main/java/com/debtgo/debtgo_backend/DebtgoBackendApplication.java
+++ src/main/java/com/debtgo/debtgo_backend/DebtgoBackendApplication.java
@@ -2,32 +2,11 @@ package com.debtgo.debtgo_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DebtgoBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DebtgoBackendApplication.class, args);
-	}
-
-	// Permite solicitudes desde el frontend Angular
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins(
-								"http://localhost:4200",
-								"https://frontend-debtogo.homeservergv.com"
-						) // frontend Angular
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.allowCredentials(true);
-			}
-		};
 	}
 }
