40731b0b9b96b06db013b56f54b810102281b05e
Developer <danielj.chavarriz@gmail.com>
2025-12-01 17:37:57 -0500

    feat(debtgo): update back-end

--- src/main/java/com/debtgo/debtgo_backend/controller/HomeController.java
+++ src/main/java/com/debtgo/debtgo_backend/controller/HomeController.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/api/home")
-@CrossOrigin(origins = "http://localhost:4200") 
+@CrossOrigin(origins = "*")
 public class HomeController {
 
     @GetMapping("/summary")
